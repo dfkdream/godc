08c9296dc20685578c876aa97543304b8bf848a5
dfkdream <dfkdream@gmail.com>
2018-08-29 01:41:06 +0900

    Add FetchArticleData function

---
+++ articleData.go
@@ -0,0 +1,258 @@
+package godc
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"regexp"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+//https://stackoverflow.com/questions/30109061/golang-parse-html-extract-all-content-with-body-body-tags
+func renderNode(n *html.Node) string {
+	var buf bytes.Buffer
+	w := io.Writer(&buf)
+	html.Render(w, n)
+	return buf.String()
+}
+
+//FetchArticleData reads article data of specific URL
+//
+//지정한 URL의 게시물을 읽어옵니다.
+func FetchArticleData(URL string) (*ArticleBody, error) {
+	dcArticle := fetchURL(URL)
+	if dcArticle == nil {
+		return nil, errors.New("Page fetch error")
+	}
+	doc, err := html.Parse(dcArticle)
+	if err != nil {
+		return nil, errors.New("html parse error")
+	}
+
+	result := ArticleBody{}
+
+	//Body 처리
+	var gallContent *html.Node
+	var searchGallContent func(*html.Node)
+	searchGallContent = func(n *html.Node) {
+		if n.Type == html.ElementNode && len(n.Attr) > 0 && n.Attr[0].Val == "gall_content" {
+			gallContent = n
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchGallContent(c)
+		}
+	}
+	searchGallContent(doc)
+
+	var searchTitle func(*html.Node)
+	searchTitle = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "tit_view" {
+			result.Title = strings.TrimSpace(n.FirstChild.Data)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchTitle(c)
+		}
+	}
+	searchTitle(gallContent)
+
+	var infoHeader *html.Node
+	var searchInfoHeader func(*html.Node)
+	searchInfoHeader = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "info_edit" {
+			infoHeader = n
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchInfoHeader(c)
+		}
+	}
+	searchInfoHeader(gallContent)
+
+	var searchName func(*html.Node)
+	vaildTimestamp := regexp.MustCompile(`\d+\.\d+\.\d+ \d+\:\d+$`)
+	searchName = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) == 0 && n.FirstChild != nil {
+			if vaildTimestamp.MatchString(n.FirstChild.Data) {
+				result.Timestamp = n.FirstChild.Data
+			} else {
+				result.Name = n.FirstChild.Data
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchName(c)
+		}
+	}
+	searchName(infoHeader)
+
+	var searchViewCounter func(*html.Node)
+	searchViewCounter = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) == 1 && n.Attr[0].Val == "num" && n.FirstChild != nil {
+			result.ViewCounter = n.FirstChild.Data
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchViewCounter(c)
+		}
+	}
+	searchViewCounter(infoHeader)
+
+	var searchReplyCount func(*html.Node)
+	searchReplyCount = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 1 && n.Attr[1].Val == "comment_dirc" && n.FirstChild != nil {
+			result.ReplyCount = n.FirstChild.Data
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchReplyCount(c)
+		}
+	}
+	searchReplyCount(infoHeader)
+
+	var searchBody func(*html.Node)
+	searchBody = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "div" && len(n.Attr) > 0 && n.Attr[0].Val == "view_main" {
+			result.Body = strings.TrimSpace(renderNode(n))
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchBody(c)
+		}
+	}
+	searchBody(gallContent)
+
+	var searchUpVote func(*html.Node)
+	searchUpVote = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "recomm_btn" && n.FirstChild != nil {
+			result.UpVote = n.FirstChild.Data
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchUpVote(c)
+		}
+	}
+	searchUpVote(gallContent)
+
+	var searchDownVote func(*html.Node)
+	searchDownVote = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "nonrecomm_btn" && n.FirstChild != nil {
+			result.DownVote = n.FirstChild.Data
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchDownVote(c)
+		}
+	}
+	searchDownVote(gallContent)
+
+	var searchIP func(*html.Node)
+	searchIP = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "ip" && n.FirstChild != nil {
+			result.IP = n.FirstChild.Data
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchIP(c)
+		}
+	}
+	searchIP(doc)
+
+	//Comment 처리
+	var gallComment *html.Node
+	var searchGallComment func(*html.Node)
+	searchGallComment = func(n *html.Node) {
+		if n.Type == html.ElementNode && len(n.Attr) > 0 && n.Attr[0].Val == "wrap_list" {
+			gallComment = n
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			searchGallComment(c)
+		}
+	}
+	searchGallComment(doc)
+
+	comments := make([]*html.Node, 0)
+	var sepComments func(*html.Node)
+	sepComments = func(n *html.Node) {
+		var vaildComment = regexp.MustCompile(`comment_cnt_[0-9]*$`)
+		if n.Type == html.ElementNode && n.Data == "li" && len(n.Attr) > 0 && vaildComment.MatchString(n.Attr[0].Val) {
+			comments = append(comments, n)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			sepComments(c)
+		}
+	}
+	sepComments(gallComment)
+
+	for _, commentNode := range comments {
+		parsedComment := Reply{}
+		var parseName func(*html.Node)
+		parseName = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) == 0 && n.FirstChild != nil {
+				parsedComment.Name = n.FirstChild.Data[1:]
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				parseName(c)
+			}
+		}
+		parseName(commentNode)
+
+		var parseIPName func(*html.Node)
+		parseIPName = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "id" && n.FirstChild != nil {
+				parsedComment.Name = n.FirstChild.Data[1:][:len(n.FirstChild.Data)-2]
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				parseIPName(c)
+			}
+		}
+		parseIPName(commentNode)
+
+		var parseURL func(*html.Node)
+		parseURL = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "a" && len(n.Attr) > 0 && n.Attr[1].Val == "id" && n.FirstChild != nil {
+				parsedComment.URL = n.Attr[0].Val
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				parseURL(c)
+			}
+		}
+		parseURL(commentNode)
+
+		var parseBody func(*html.Node)
+		parseBody = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "txt" && n.FirstChild != nil {
+				parsedComment.Body = strings.TrimSpace(renderNode(n.FirstChild))
+			}
+			if parsedComment.Body == "" {
+				if n.Type == html.ElementNode && n.Data == "img" {
+					parsedComment.Body = strings.TrimSpace(renderNode(n))
+				}
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				parseBody(c)
+			}
+		}
+		parseBody(commentNode)
+
+		var parseTimestamp func(*html.Node)
+		parseTimestamp = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "date" && n.FirstChild != nil {
+				parsedComment.Timestamp = n.FirstChild.Data
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				parseTimestamp(c)
+			}
+		}
+		parseTimestamp(commentNode)
+
+		var parseIP func(*html.Node)
+		parseIP = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "span" && len(n.Attr) > 0 && n.Attr[0].Val == "ip" && n.FirstChild != nil {
+				parsedComment.IP = n.FirstChild.Data
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				parseIP(c)
+			}
+		}
+		parseIP(commentNode)
+
+		result.Replies = append(result.Replies, parsedComment)
+	}
+
+	return &result, nil
+
+}
--- type.go
+++ type.go
@@ -1,6 +1,6 @@
 package godc
 
-//ArticleData contains post informations.
+//ArticleData contains post(list) informations.
 type ArticleData struct {
 	URL         string
 	Title       string
@@ -11,4 +11,27 @@ type ArticleData struct {
 	ViewCounter string
 	UpVote      string
 	WriterID    string
+}
+
+//ArticleBody contains article informations.
+type ArticleBody struct {
+	Title       string
+	Name        string
+	IP          string
+	Timestamp   string
+	ViewCounter string
+	ReplyCount  string
+	Body        string
+	UpVote      string
+	DownVote    string
+	Replies     []Reply
+}
+
+//Reply contains reply data of articleBody.
+type Reply struct {
+	URL       string
+	Name      string
+	IP        string
+	Body      string
+	Timestamp string
 }
